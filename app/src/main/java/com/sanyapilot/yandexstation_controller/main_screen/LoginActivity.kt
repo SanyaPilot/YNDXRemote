f5977041221a66381e557ebcbe3b54a90a0039c7
Alexander Baransky <alexander.baranskiy@yandex.ru>
2023-08-15 13:26:58 +0300

    LoginActivity: Rework login screen Lock orientation for LoginActivity to portrait (not optimized for a landscape)

--- app/src/main/java/com/sanyapilot/yandexstation_controller/main_screen/LoginActivity.kt
+++ app/src/main/java/com/sanyapilot/yandexstation_controller/main_screen/LoginActivity.kt
@@ -3,9 +3,10 @@ package com.sanyapilot.yandexstation_controller.main_screen
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputLayout
@@ -32,33 +33,54 @@ class LoginActivity : AppCompatActivity() {
         }
 
         codeField = findViewById<TextInputLayout>(R.id.codeField).editText!!
-    }
-    fun auth(view: View) {
-        thread(start = true) {
-            val res = Session.login(codeField.text.toString().toInt())
-            if (!res.ok) {
-                val stringID = when (res.errorId) {
-                    Errors.INVALID_CODE -> R.string.loginInvalidCode
-                    Errors.INTERNAL_SERVER_ERROR -> R.string.serverDead
-                    else -> R.string.wtf
-                }
 
-                runOnUiThread {
-                    Snackbar.make(
-                        findViewById(R.id.loginLayout), getString(stringID),
-                        Snackbar.LENGTH_LONG
-                    ).show()
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        val howToCodeButton = findViewById<Button>(R.id.howToCodeButton)
+        val telegramButton = findViewById<Button>(R.id.telegramButton)
+
+        loginButton.setOnClickListener {
+            thread(start = true) {
+                if (codeField.text.isEmpty()) {
+                    runOnUiThread {
+                        Snackbar.make(
+                            findViewById(R.id.loginLayout), getString(R.string.loginInvalidCode),
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                } else {
+                    val res = Session.login(codeField.text.toString().toInt())
+                    if (!res.ok) {
+                        val stringID = when (res.errorId) {
+                            Errors.INVALID_CODE -> R.string.loginInvalidCode
+                            Errors.INTERNAL_SERVER_ERROR -> R.string.serverDead
+                            else -> R.string.wtf
+                        }
+
+                        runOnUiThread {
+                            Snackbar.make(
+                                findViewById(R.id.loginLayout), getString(stringID),
+                                Snackbar.LENGTH_LONG
+                            ).show()
+                        }
+                        return@thread
+                    }
+                    with(sharedPrefs.edit()) {
+                        putString("access-token", Session.accessToken)
+                        apply()
+                    }
+                    runOnUiThread {
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    }
                 }
-                return@thread
             }
-            with(sharedPrefs.edit()) {
-                putString("access-token", Session.accessToken)
-                apply()
-            }
-            runOnUiThread {
-                startActivity(Intent(this, MainActivity::class.java))
-                finish()
-            }
+        }
+
+        howToCodeButton.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://yndxfuck.ru/remote")))
+        }
+        telegramButton.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/yndxfuck")))
         }
     }
 }
